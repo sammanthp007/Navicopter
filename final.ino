caffa0c230d27df141faea97f0685c697f719cf7
sammanthp007 <samman.thapa@bison.howard.edu>
2016-05-03 12:20:01 -0400

    change final

--- final.ino
+++ final.ino
@@ -1,5 +1,5 @@
 const int controlPin = 9;
-int startWait = 20000;
+int startWait = 2000;
 int afterStartWait = startWait + 2000;
 int addTimeValue = 2000;
 
@@ -29,14 +29,17 @@ void loop()
 {
   Serial.print("\nInitial Value\n");
   // put your main code here, to run repeatedly:
-  for(int fadeValue = 110 ; fadeValue <= 205; fadeValue +=1) { 
+  for(int fadeValue = 110 ; fadeValue <= 190; fadeValue +=1) { 
     // sets the value (range from 0 to 255):
     Serial.print("fadeValue = ");
     Serial.print(fadeValue);
     Serial.print("\n");
     analogWrite(controlPin, fadeValue);         
     // wait for 30 milliseconds to see the dimming effect    
-    delay(20);                         
-  } 
+    delay(50);                         
+  }
+// while (true) {
+//     analogWrite(controlPin, 150);
+//   } 
 }
 
